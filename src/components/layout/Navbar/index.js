959f51dc772001a1b83d7867f8a262982e9eb6ef
Francisco Lopez <lopezf28@gmail.com>
2024-12-10 00:31:17 -0300

    Corriginedo index.js

--- src/components/layout/Navbar/index.js
+++ src/components/layout/Navbar/index.js
@@ -1,42 +1,31 @@
-//Contenido para los archivos index.js de los componentes
-
-//Layout
-cat > src / components / layout / Navbar / index.js << EOL
 export { Navbar } from './Navbar';
 export { NavLink } from './NavLink';
 export { MobileNavLink } from './MobileNavLink';
-EOL
 
-cat > src / components / layout / Footer / index.js << EOL
 export { Footer } from './Footer';
-EOL
 
-//Common
-cat > src / components / common / SocialLinks / index.js << EOL
 export { SocialLinks } from './SocialLinks';
 export { SocialLink } from './SocialLink';
-EOL
 
-//Sections
-cat > src / components / sections / Hero / index.js << EOL
 export { Hero } from './Hero';
-EOL
 
-cat > src / components / sections / About / index.js << EOL
 export { About } from './About';
-EOL
 
-cat > src / components / sections / Projects / index.js << EOL
 export { Projects } from './Projects';
 export { ProjectCard } from './ProjectCard';
-EOL
 
-cat > src / components / sections / Skills / index.js << EOL
 export { Skills } from './Skills';
 export { SkillCategory } from './SkillCategory';
-EOL
 
-cat > src / components / sections / Contact / index.js << EOL
 export { Contact } from './Contact';
 export { ContactForm } from './ContactForm';
-EOL
+
+export * from './layout/Navbar';
+export * from './layout/Footer';
+export * from './common/SocialLinks';
+export * from './sections/Hero';
+export * from './sections/About';
+export * from './sections/Projects';
+export * from './sections/Skills';
+export * from './sections/Contact';
+
