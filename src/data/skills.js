3b249a0a30cde29036da831f2aece42da2200acd
Francisco Lopez <lopezf28@gmail.com>
2024-12-10 00:16:22 -0300

    Creando skills.js

---
+++ src/data/skills.js
@@ -0,0 +1,19 @@
+// src/data/skills.js
+export const skillsData = [
+    {
+        categoria: "Frontend",
+        skills: ["HTML5", "CSS3", "JavaScript (ES6+)", "React", "TypeScript"]
+    },
+    {
+        categoria: "Frameworks CSS",
+        skills: ["Tailwind CSS", "Bootstrap", "Sass"]
+    },
+    {
+        categoria: "Herramientas",
+        skills: ["Git", "Webpack", "Jest", "npm"]
+    },
+    {
+        categoria: "Diseño",
+        skills: ["Figma", "Adobe XD", "Responsive Design"]
+    }
+];
